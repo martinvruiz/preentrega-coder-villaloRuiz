d6866307783bc8e9f4d33127e5baaeca924e7992
martinvruiz <martinvruiz10@gmail.com>
2024-09-03 20:00:49 -0300

    added images to articles

--- src/components/ItemListContainer/components/ItemDetailContainer/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemListContainer/components/ItemDetailContainer/components/ItemDetail/ItemDetail.jsx
@@ -2,7 +2,7 @@ import {ItemCount} from "../../../Item/components/ItemCount/ItemCount"
 
 export const ItemDetail = ({product})=>{
     return <section className="flex justify-center items-center">
-            <div className="flex flex-col items-center w-72 mx-2 my-2 h-64 p-3 border border-cyan-700 rounded-md">
+            <div className="flex flex-col justify-center items-center w-96 mx-2 my-10 h-80 p-3 border border-cyan-700 rounded-md">
             <img className="object-contain w-1/2 h-28" src={product.img} alt={product.title}/>
             <h4 className="font-bold">{product.title}</h4>
             <p>{product.price}</p>
--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Nosotros() {
   return (
-    <div>Nosotros</div>
+    <div className='flex flex-col items-center my-8'>
+      <h2 className='font-bold text-4xl'>Nosotros</h2>
+      <p className='px-10 mt-3 text-center text-xl font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure libero voluptates ipsa sint amet ducimus at deleniti, unde necessitatibus alias totam dolore quisquam. Suscipit quo impedit facere exercitationem ducimus sequi accusantium vitae, ad earum iure quis perferendis at, quibusdam quae totam dignissimos recusandae et doloremque iusto quidem commodi fugiat consequatur perspiciatis. Dicta, ipsa quia nulla modi qui, deleniti cumque repudiandae delectus eligendi dignissimos eius voluptatum? Debitis, error deserunt obcaecati temporibus beatae nisi iusto delectus culpa vel minus similique maiores, iure quo non mollitia? Nulla porro atque dolores, neque, natus blanditiis eos iste hic dolorem deserunt dignissimos voluptates autem, pariatur rem.</p>
+      <p className='px-10 mt-3 text-center text-xl font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure libero voluptates ipsa sint amet ducimus at deleniti, unde necessitatibus alias totam dolore quisquam. Suscipit quo impedit facere exercitationem ducimus sequi accusantium vitae, ad earum iure quis perferendis at, quibusdam quae totam dignissimos recusandae et doloremque iusto quidem commodi fugiat consequatur perspiciatis. Dicta, ipsa quia nulla modi qui, deleniti cumque repudiandae delectus eligendi dignissimos eius voluptatum? Debitis, error deserunt obcaecati temporibus beatae nisi iusto delectus culpa vel minus similique maiores, iure quo non mollitia? Nulla porro atque dolores, neque, natus blanditiis eos iste hic dolorem deserunt dignissimos voluptates autem, pariatur rem.</p>
+    </div>
   )
 }
